2f52ac64d5e2e186f700218e0402cd101c9d449e
F1st3K <kostinnikita988@gmail.com>
2022-11-08 21:00:59 +0300

    refactor, add ConsoleOutput

--- ConsolePixelationTool/Program.cs
+++ ConsolePixelationTool/Program.cs
@@ -18,7 +18,7 @@ namespace ConsolePixelationTool
             System.Drawing.Image png = System.Drawing.Image.FromStream(fs);
             fs.Close();
             Bitmap img = new Bitmap(png);
-            Screen screen = new SymbolScreen(img.Height, img.Width, 2);
+            Screen screen = new Screen(img.Height, img.Width, 2);
             screen.ChangeScreen(img);
             screen.DrawScreen(0, 0);
             //screen.RGBTest();
